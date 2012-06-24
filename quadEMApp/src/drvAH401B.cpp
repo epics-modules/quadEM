9e56bc7794ac056ccd4e5ea4dd26544f26e92085
Mark Rivers <MarkRivers@users.noreply.github.com>
2012-06-24 17:22:26 +0000

    Many changes while developing

--- quadEMApp/src/drvAH401B.cpp
+++ quadEMApp/src/drvAH401B.cpp
@@ -29,7 +29,7 @@
 
 
 static const char *driverName="drvAH401B";
-void readThread(void *drvPvt);
+static void readThread(void *drvPvt);
 
 
 /** Constructor for the drvAH401B class.
@@ -44,6 +44,8 @@ drvAH401B::drvAH401B(const char *portName, const char *QEPortName)
     asynStatus status;
     const char *functionName = "drvAH401B";
     
+    QEPortName_ = epicsStrDup(QEPortName);
+    
     readDataEvent_ = epicsEventCreate(epicsEventEmpty);
     
     // Connect to the server
@@ -54,6 +56,12 @@ drvAH401B::drvAH401B(const char *portName, const char *QEPortName)
         return;
     }
 
+    setAcquire(0);
+    setIntegerParam(P_Acquire, 0);
+    strcpy(outString_, "VER ?");
+    writeReadMeter();
+    firmwareVersion_ = epicsStrDup(inString_);
+    
     /* Create the thread that reads the meter */
     status = (asynStatus)(epicsThreadCreate("drvAH401BTask",
                           epicsThreadPriorityMedium,
@@ -68,28 +76,46 @@ drvAH401B::drvAH401B(const char *portName, const char *QEPortName)
 
 
 
-void readThread(void *drvPvt)
+static void readThread(void *drvPvt)
 {
     drvAH401B *pPvt = (drvAH401B *)drvPvt;
     
     pPvt->readThread();
 }
 
+/** Sends a command to the AH401B and reads the response.
+  * If meter is acquiring turns it off and waits for it to stop sending data */ 
+asynStatus drvAH401B::sendCommand()
+{
+  int prevAcquiring;
+  asynStatus status;
+  const char *functionName="sendCommand";
+  
+  prevAcquiring = acquiring_;
+  if (prevAcquiring) setAcquire(0);
+  status = writeReadMeter();
+  if (strcmp(inString_, "ACK") != 0) {
+      asynPrint(pasynUserSelf, ASYN_TRACE_ERROR, 
+          "%s:%s: error, expected ACK, received %s\n",
+          driverName, functionName, inString_);
+  }
+  if (prevAcquiring) setAcquire(1);
+  return status;
+}
+
+
 /** Writes a string to the AH401B and reads the response.
-  * Calls writeReadController() with default locations of the input and output strings
-  * and default timeout. */ 
+  * If meter is acquiring turns it off and waits for it to stop sending data */ 
 asynStatus drvAH401B::writeReadMeter()
 {
   size_t nread;
   size_t nwrite;
   asynStatus status;
   int eomReason;
-  // const char *functionName="writeReadController";
+  //const char *functionName="writeReadMeter";
   
-  status = pasynOctetSyncIO->writeRead(pasynUserMeter_, outString_,
-                                       strlen(outString_), inString_, sizeof(inString_), AH401B_TIMEOUT,
-                                       &nwrite, &nread, &eomReason);
-                        
+  status = pasynOctetSyncIO->writeRead(pasynUserMeter_, outString_, strlen(outString_), 
+                                       inString_, sizeof(inString_), AH401B_TIMEOUT, &nwrite, &nread, &eomReason);                                      
   return status;
 }
 
@@ -104,6 +130,7 @@ void drvAH401B::readThread(void)
     size_t nRead; 
     int eomReason;
     epicsInt32 raw[QE_MAX_INPUTS];
+    char input[MAX_COMMAND_LEN];
     double integrationTime;
     static const char *functionName = "readThread";
     
@@ -118,29 +145,47 @@ void drvAH401B::readThread(void)
         }
         getDoubleParam(P_IntegrationTime, &integrationTime);
         unlock();
-        status = pasynOctetSyncIO->read(pasynUserMeter_, inString_, sizeof(inString_), 
-                                        integrationTime/2., &nRead, &eomReason);
+        status = pasynOctetSyncIO->read(pasynUserMeter_, input, sizeof(input), 
+                                        AH401B_TIMEOUT, &nRead, &eomReason);
         lock();
-        if (status == asynTimeout) continue;
+        if (nRead == 0) continue;
         if (status != asynSuccess) {
             asynPrint(pasynUserSelf, ASYN_TRACE_ERROR, 
-                "%s:%s: unexpected error reading meter status=%d\n", 
+                "%s:%s: unexpected error reading meter status=%d, nRead=%d, eomReason=%d\n", 
                 driverName, functionName, status);
             continue;
         }
-        sscanf(inString_, "%d %d %d %d", &raw[0], &raw[1], &raw[2], &raw[3]);
+        sscanf(input, "%d %d %d %d", &raw[0], &raw[1], &raw[2], &raw[3]);
         computePositions(raw);
     }
 }
 
 asynStatus drvAH401B::setAcquire(epicsInt32 value) 
 {
-    asynStatus status;
-    
-    epicsSnprintf(outString_, sizeof(outString_), "ACQ %s", value ? "ON" : "OFF");
-    status = writeReadMeter();
-    // Notify the read thread if acquisition status has started
-    if (value) epicsEventSignal(readDataEvent_);
+    size_t nread;
+    size_t nwrite;
+    asynStatus status=asynSuccess, readStatus;
+    int eomReason;
+    char dummyIn[MAX_COMMAND_LEN];
+
+    if (value == 0) {
+        while (1) {
+            // Repeat sending ACQ OFF until we get only an ACK back
+            status = pasynOctetSyncIO->writeRead(pasynUserMeter_, "ACQ OFF", strlen("ACQ OFF"), 
+                                             dummyIn, MAX_COMMAND_LEN, AH401B_TIMEOUT, &nwrite, &nread, &eomReason);
+            // Flush the input buffer because there could be more characters sent after ACK
+            nread = 0;
+            readStatus = pasynOctetSyncIO->read(pasynUserMeter_, dummyIn, MAX_COMMAND_LEN, .1, &nread, &eomReason);
+            if ((readStatus == asynTimeout) && (nread == 0)) break;
+        }
+        acquiring_ = 0;
+    } else {
+        status = pasynOctetSyncIO->writeRead(pasynUserMeter_, "ACQ ON", strlen("ACQ ON"), 
+                                             dummyIn, MAX_COMMAND_LEN, AH401B_TIMEOUT, &nwrite, &nread, &eomReason);
+        // Notify the read thread if acquisition status has started
+        epicsEventSignal(readDataEvent_);
+        acquiring_ = 1;
+    }
     return status;
 }
 
@@ -149,8 +194,8 @@ asynStatus drvAH401B::setPingPong(epicsInt32 value)
 {
     asynStatus status;
     
-    epicsSnprintf(outString_, sizeof(outString_), "HLF %s", value ? "ON" : "OFF");
-    status = writeReadMeter();
+    epicsSnprintf(outString_, sizeof(outString_), "HLF %s", value ? "OFF" : "ON");
+    status = sendCommand();
     return status;
 }
 
@@ -164,7 +209,7 @@ asynStatus drvAH401B::setIntegrationTime(epicsFloat64 value)
         setDoubleParam(P_IntegrationTime, value);
     }
     epicsSnprintf(outString_, sizeof(outString_), "ITM %d", (int)(value * 10000));
-    status = writeReadMeter();
+    status = sendCommand();
     return status;
 }
 
@@ -173,7 +218,7 @@ asynStatus drvAH401B::setRange(epicsInt32 value)
     asynStatus status;
     
     epicsSnprintf(outString_, sizeof(outString_), "RNG %d", value);
-    status = writeReadMeter();
+    status = sendCommand();
     return status;
 }
 
@@ -188,10 +233,63 @@ asynStatus drvAH401B::setTrigger(epicsInt32 value)
     asynStatus status;
     
     epicsSnprintf(outString_, sizeof(outString_), "TRG %s", value ? "ON" : "OFF");
-    status = writeReadMeter();
+    status = sendCommand();
     return status;
 }
 
+asynStatus drvAH401B::getSettings() 
+{
+    // Reads the values of all the meter parameters, sets them in the parameter library
+    int pingPong, trigger, range;
+    double integrationTime;
+    int prevAcquiring;
+    static const char *functionName = "getStatus";
+    
+    prevAcquiring = acquiring_;
+    if (prevAcquiring) setAcquire(0);
+    strcpy(outString_, "TRG ?");
+    writeReadMeter();
+    if (strcmp("TRG ON", inString_) == 0) trigger = 1;
+    else if (strcmp("TRG OFF", inString_) == 0) trigger = 0;
+    else goto error;
+    setIntegerParam(P_Trigger, trigger);
+    
+    strcpy(outString_, "HLF ?");
+    writeReadMeter();
+    if (strcmp("HLF ON", inString_) == 0) pingPong = 0;
+    else if (strcmp("HLF OFF", inString_) == 0) pingPong = 1;
+    else goto error;
+    setIntegerParam(P_PingPong, pingPong);
+    
+    strcpy(outString_, "RNG ?");
+    writeReadMeter();
+    if (sscanf(inString_, "RNG %d", &range) != 1) goto error;
+    setIntegerParam(P_Range, range);
+    
+    strcpy(outString_, "ITM ?");
+    writeReadMeter();
+    if (sscanf(inString_, "ITM %lf", &integrationTime) != 1) goto error;
+    integrationTime = integrationTime/10000.;
+    setDoubleParam(P_IntegrationTime, integrationTime);
+    
+    if (prevAcquiring) setAcquire(1);
+    
+    return asynSuccess;
+
+    error:
+    asynPrint(pasynUserSelf, ASYN_TRACE_ERROR,
+        "%s:%s: error, outString=%s, inString=%s\n",
+        driverName, functionName, outString_, inString_);
+    return asynError;
+}
+
+
+void drvAH401B::report(FILE *fp, int details)
+{
+    fprintf(fp, "%s: port=%s, IP port=%s, firmware version=%s\n",
+            driverName, portName, QEPortName_, firmwareVersion_);
+    drvQuadEM::report(fp, details);
+}
 
 
 /* Configuration routine.  Called directly, or from the iocsh function below */
