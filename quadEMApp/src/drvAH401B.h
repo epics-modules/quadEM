86d9097b61b4505d7e2f4315b06fec8770d11174
Mark Rivers <MarkRivers@users.noreply.github.com>
2012-06-24 17:22:04 +0000

    Many changes while developing

--- quadEMApp/src/drvAH401B.h
+++ quadEMApp/src/drvAH401B.h
@@ -19,24 +19,33 @@
 class drvAH401B : public drvQuadEM {
 public:
     drvAH401B(const char *portName, const char *QEPortName);
+    
+    /* These are the methods we implement from asynPortDriver */
+    void report(FILE *fp, int details);
                  
     /* These are the methods that are new to this class */
     void readThread(void);
 
 protected:
+    /* These are the methods we implement from quadEM */
     virtual asynStatus setAcquire(epicsInt32 value);
     virtual asynStatus setPingPong(epicsInt32 value);
     virtual asynStatus setIntegrationTime(epicsFloat64 value);
     virtual asynStatus setRange(epicsInt32 value);
     virtual asynStatus setReset();
     virtual asynStatus setTrigger(epicsInt32 value);
+    virtual asynStatus getSettings();
  
 private:
     /* Our data */
     asynUser *pasynUserMeter_;
     epicsEventId readDataEvent_;
+    int acquiring_;
+    char *QEPortName_;
+    char *firmwareVersion_;
     char outString_[MAX_COMMAND_LEN];
     char inString_[MAX_COMMAND_LEN];
+    asynStatus sendCommand();
     asynStatus writeReadMeter();
 };
 
