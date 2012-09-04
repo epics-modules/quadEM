51544dcebb6ad0f4cc06a04fe26fccd37ce5ecf7
Mark Rivers <MarkRivers@users.noreply.github.com>
2012-09-04 19:02:41 +0000

    Supported setReset command to download all settings; fixed comments for Doxygen

--- quadEMApp/src/drvAH401B.cpp
+++ quadEMApp/src/drvAH401B.cpp
@@ -33,7 +33,7 @@ static void readThread(void *drvPvt);
 
 
 /** Constructor for the drvAH401B class.
-  * Calls constructor for the asynPortDriver base class.
+  * Calls the constructor for the drvQuadEM base class.
   * \param[in] portName The name of the asyn port driver to be created.
   * \param[in] QEPortName The name of the asyn communication port to the AH401B 
   *            created with drvAsynIPPortConfigure or drvAsynSerialPortConfigure */
@@ -61,9 +61,8 @@ drvAH401B::drvAH401B(const char *portName, const char *QEPortName)
     strcpy(outString_, "VER ?");
     writeReadMeter();
     firmwareVersion_ = epicsStrDup(inString_);
-    strcpy(outString_, "BIN ON");
-    writeReadMeter();
-    
+    setBinaryMode();
+
     // Read the current settings from the device.  This will set parameters in the parameter library.
     getSettings();
     
@@ -125,11 +124,11 @@ asynStatus drvAH401B::writeReadMeter()
 }
 
 
-/** Read thread to read the data from the electrometer when it is in continuous acquire mode */
+/** Read thread to read the data from the electrometer when it is in continuous acquire mode.
+  * Reads the data, computes the sums and positions, and does callbacks.
+  */
 void drvAH401B::readThread(void)
 {
-    /* This thread reads the data, computes the sums and positions, and does callbacks */
-
     int acquire;
     asynStatus status;
     size_t nRead; 
@@ -172,6 +171,9 @@ void drvAH401B::readThread(void)
     }
 }
 
+/** Starts and stops the electrometer.
+  * \param[in] value 1 to start the electrometer, 0 to stop it.
+  */
 asynStatus drvAH401B::setAcquire(epicsInt32 value) 
 {
     size_t nread;
@@ -204,6 +206,9 @@ asynStatus drvAH401B::setAcquire(epicsInt32 value)
 }
 
  
+/** Sets the ping-pong setting. 
+  * \param[in] value 0: use both ping and pong (HLF OFF), value=1: use just ping (HLF ON) 
+  */
 asynStatus drvAH401B::setPingPong(epicsInt32 value) 
 {
     asynStatus status;
@@ -213,6 +218,9 @@ asynStatus drvAH401B::setPingPong(epicsInt32 value)
     return status;
 }
 
+/** Sets the integration time. 
+  * \param[in] value The integration time in seconds [0.001 - 1.000]
+  */
 asynStatus drvAH401B::setIntegrationTime(epicsFloat64 value) 
 {
     asynStatus status;
@@ -227,6 +235,9 @@ asynStatus drvAH401B::setIntegrationTime(epicsFloat64 value)
     return status;
 }
 
+/** Sets the range 
+  * \param[in] value The desired range.
+  */
 asynStatus drvAH401B::setRange(epicsInt32 value) 
 {
     asynStatus status;
@@ -236,12 +247,48 @@ asynStatus drvAH401B::setRange(epicsInt32 value)
     return status;
 }
 
+/** Downloads all of the current EPICS settings to the electrometer.  
+  * Typically used after the electrometer is power-cycled.
+  */
 asynStatus drvAH401B::setReset() 
 {
-    // Not supported on AH401B, just ignore
+    epicsInt32 iValue;
+    epicsFloat64 dValue;
+
+    getIntegerParam(P_Acquire, &iValue);
+    setAcquire(iValue);
+
+    getIntegerParam(P_PingPong, &iValue);
+    setPingPong(iValue);
+
+    getDoubleParam(P_IntegrationTime, &dValue);
+    setIntegrationTime(dValue);
+
+    getIntegerParam(P_Range, &iValue);
+    setRange(iValue);
+
+    getIntegerParam(P_Trigger, &iValue);
+    setTrigger(iValue);
+    
+    setBinaryMode();
+
+    getSettings();
+    
     return asynSuccess;
 }
 
+/** Put the electrometer in binary mode, which is the only mode this driver uses.
+  */
+asynStatus drvAH401B::setBinaryMode()
+{
+    strcpy(outString_, "BIN ON");
+    writeReadMeter();
+    return asynSuccess;
+}
+
+/** Sets the trigger mode.
+  * \param[in] value 0=Internal trigger, 1=External trigger.
+  */
 asynStatus drvAH401B::setTrigger(epicsInt32 value) 
 {
     asynStatus status;
@@ -251,6 +298,8 @@ asynStatus drvAH401B::setTrigger(epicsInt32 value)
     return status;
 }
 
+/** Reads all the settings back from the electrometer.
+  */
 asynStatus drvAH401B::getSettings() 
 {
     // Reads the values of all the meter parameters, sets them in the parameter library
@@ -301,6 +350,10 @@ asynStatus drvAH401B::getSettings()
 }
 
 
+/** Report  parameters 
+  * \param[in] fp The file pointer to write to
+  * \param[in] details The level of detail requested
+  */
 void drvAH401B::report(FILE *fp, int details)
 {
     fprintf(fp, "%s: port=%s, IP port=%s, firmware version=%s\n",
@@ -315,7 +368,8 @@ extern "C" {
 
 /** EPICS iocsh callable function to call constructor for the drvAH401B class.
   * \param[in] portName The name of the asyn port driver to be created.
-  * \param[in] maxPoints The maximum  number of points in the volt and time arrays */
+  * \param[in] QEPortName The name of the asyn communication port to the AH401B 
+  *            created with drvAsynIPPortConfigure or drvAsynSerialPortConfigure */
 int drvAH401BConfigure(const char *portName, const char *QEPortName)
 {
     new drvAH401B(portName, QEPortName);
