83eaf7ba327f39482c0f7ba152b13eee310d5287
Mark Rivers <MarkRivers@users.noreply.github.com>
2013-12-03 18:43:55 +0000

    Document new modelName parameter

--- quadEMApp/src/drvAHxxx.cpp
+++ quadEMApp/src/drvAHxxx.cpp
@@ -45,6 +45,9 @@ static void readThread(void *drvPvt);
   *            This should be large enough to hold all the samples between reads of the
   *            device, e.g. 1 ms SampleTime and 1 second read rate = 1000 samples.
   *            If 0 then default of 2048 is used.
+  * \param[in] modelName The model of electrometer.  It is too difficult to try to determine
+  *            this from the firmware version number, so it must be specified. Allowed values are:
+  *            "AH401B", "AH401D", "AH501", "AH501C", and "AH501D".
   */
 drvAHxxx::drvAHxxx(const char *portName, const char *QEPortName, int ringBufferSize, const char *modelName) 
    : drvQuadEM(portName, 0, ringBufferSize)
@@ -56,7 +59,7 @@ drvAHxxx::drvAHxxx(const char *portName, const char *QEPortName, int ringBufferS
     QEPortName_ = epicsStrDup(QEPortName);
     
     acquireStartEvent_ = epicsEventCreate(epicsEventEmpty);
-    
+
     // Connect to the server
     status = pasynOctetSyncIO->connect(QEPortName, 0, &pasynUserMeter_, NULL);
     if (status) {
