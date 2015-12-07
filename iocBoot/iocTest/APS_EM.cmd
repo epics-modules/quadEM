217a85e9e4e22fde580fd2743e78441f73d5136c
Mark Rivers <rivers@cars.uchicago.edu>
2015-12-07 08:53:37 -0600

    Added all the commands that need to come after loading commonPlugins.cmd

--- iocBoot/iocTest/APS_EM.cmd
+++ iocBoot/iocTest/APS_EM.cmd
@@ -48,3 +48,25 @@ dbLoadRecords("$(QUADEM)/db/quadEM_TimeSeries.template", "P=$(PREFIX),R=$(RECORD
 #dbLoadTemplate("quadEM_pid.substitutions")
 
 < commonPlugins.cmd
+
+asynSetTraceIOMask("$(PORT)",0,2)
+#asynSetTraceMask("$(PORT)",  0,9)
+
+# initFastSweep(portName, inputName, maxSignals, maxPoints)
+#  portName = asyn port name for this new port (string)
+#  inputName = name of asynPort providing data
+#  maxSignals  = maximum number of signals (spectra)
+#  maxPoints  = maximum number of channels per spectrum
+#  dataString  = drvInfo string for current and position data
+#  intervalString  = drvInfo string for time interval per point
+initFastSweep("$(PORT)TS", "$(PORT)", 11, 2048, "QE_INT_ARRAY_DATA", "QE_SAMPLE_TIME")
+dbLoadRecords("$(QUADEM)/db/quadEM_TimeSeries.template", "P=$(PREFIX),R=$(RECORD)_TS:,NUM_TS=2048,NUM_FREQ=1024,PORT=$(PORT)TS")
+
+< saveRestore.cmd
+
+iocInit()
+
+# save settings every thirty seconds
+create_monitor_set("auto_settings.req",30,"P=$(PREFIX), R=$(RECORD):, R_TS=$(RECORD)_TS:")
+
+seq("quadEM_SNL", "P=$(PREFIX), R=$(RECORD)_TS:, NUM_CHANNELS=2048")
