ddf6aa80310cd69ecacfa718848021ffd8761371
Mark Rivers <rivers@cars.uchicago.edu>
2015-12-09 10:39:00 -0600

    Added note about changing path separator on Windows

--- iocBoot/iocTest/st.cmd
+++ iocBoot/iocTest/st.cmd
@@ -7,6 +7,7 @@ dbLoadDatabase("../../dbd/quadEMTestApp.dbd")
 quadEMTestApp_registerRecordDeviceDriver(pdbbase)
 
 # The search path for database files
+# Note: the separator between the path entries needs to be changed to a semicolon (;) on Windows
 epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db:$(QUADEM)/db")
 
 #< AH401B.cmd
