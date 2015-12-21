4f435f5cd5330188501f6b0fa6352cc6c62be198
Mark Rivers <rivers@cars.uchicago.edu>
2015-12-21 14:57:54 -0600

    AHxxx.cmd and saveRestore.cmd moved up 1 level

--- iocBoot/iocAH401B/AH401B.cmd
+++ iocBoot/iocAH401B/AH401B.cmd
@@ -8,11 +8,11 @@ epicsEnvSet("RING_SIZE", "10000")
 epicsEnvSet("TSPOINTS",  "1000")
 epicsEnvSet("IP",        "164.54.160.242:10001")
 
-< AHxxx.cmd
+< ../AHxxx.cmd
 
 dbLoadRecords("$(QUADEM)/db/AH401B.template", "P=$(PREFIX), R=$(RECORD):, PORT=$(PORT)")
 
-< saveRestore.cmd
+< ../saveRestore.cmd
 
 iocInit()
 
