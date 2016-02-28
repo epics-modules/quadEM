6672426f5bd76fc89e47ec64cf598fa7a2f84fcc
Mark Rivers <rivers@cars.uchicago.edu>
2016-02-28 08:10:03 -0600

    Remove FastSweep stuff, no longer needed, now done with NDPluginTimeSeries

--- iocBoot/iocTetrAMM/TetrAMM.cmd
+++ iocBoot/iocTetrAMM/TetrAMM.cmd
@@ -4,7 +4,7 @@ epicsEnvSet("PORT",      "TetrAMM")
 epicsEnvSet("TEMPLATE",  "TetrAMM")
 epicsEnvSet("QSIZE",     "20")
 epicsEnvSet("RING_SIZE", "10000")
-epicsEnvSet("TSPOINTS",  "1000")
+epicsEnvSet("TSPOINTS",  "2048")
 epicsEnvSet("IP",        "gse-tetramm2:10001")
 
 #drvAsynIPPortConfigure("portName","hostInfo",priority,noAutoConnect,
@@ -30,21 +30,9 @@ dbLoadRecords("$(QUADEM)/db/$(TEMPLATE).template", "P=$(PREFIX), R=$(RECORD):, P
 asynSetTraceIOMask("$(PORT)",0,2)
 #asynSetTraceMask("$(PORT)",  0,9)
 
-# initFastSweep(portName, inputName, maxSignals, maxPoints)
-#  portName = asyn port name for this new port (string)
-#  inputName = name of asynPort providing data
-#  maxSignals  = maximum number of signals (spectra)
-#  maxPoints  = maximum number of channels per spectrum
-#  dataString  = drvInfo string for current and position data
-#  intervalString  = drvInfo string for time interval per point
-initFastSweep("$(PORT)TS", "$(PORT)", 11, 2048, "QE_INT_ARRAY_DATA", "QE_SAMPLE_TIME")
-dbLoadRecords("$(QUADEM)/db/quadEM_TimeSeries.template", "P=$(PREFIX),R=$(RECORD)_TS:,NUM_TS=2048,NUM_FREQ=1024,PORT=$(PORT)TS")
-
 < $(QUADEM)/iocBoot/saveRestore.cmd
 
 iocInit()
 
 # save settings every thirty seconds
 create_monitor_set("auto_settings.req",30,"P=$(PREFIX), R=$(RECORD):, R_TS=$(RECORD)_TS:")
-
-seq("quadEM_SNL", "P=$(PREFIX), R=$(RECORD)_TS:, NUM_CHANNELS=2048")
