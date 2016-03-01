13f4ca02a49f76a8806c11057b9a3cfb35d192e6
Mark Rivers <rivers@cars.uchicago.edu>
2016-03-01 11:09:43 -0600

    Put colon in RECORD definition; remove seq command, changes for new NDPluginTimesSeries

--- iocBoot/iocNSLS_EM/NSLS_EM.cmd
+++ iocBoot/iocNSLS_EM/NSLS_EM.cmd
@@ -1,5 +1,5 @@
 epicsEnvSet("PREFIX",    "quadEMTest:")
-epicsEnvSet("RECORD",    "NSLS_EM")
+epicsEnvSet("RECORD",    "NSLS_EM:")
 epicsEnvSet("PORT",      "NSLS_EM")
 epicsEnvSet("TEMPLATE",  "NSLS_EM")
 epicsEnvSet("QSIZE",     "20")
@@ -20,28 +20,16 @@ asynSetTraceIOMask("TCP_Command_$(PORT)", 0, 2)
 asynSetTraceIOMask("TCP_Data_$(PORT)", 0, 2)
 #asynSetTraceMask("TCP_Data_$(PORT)", 0, 9)
 
-dbLoadRecords("$(QUADEM)/db/$(TEMPLATE).template", "P=$(PREFIX), R=$(RECORD):, PORT=$(PORT)")
+dbLoadRecords("$(QUADEM)/db/$(TEMPLATE).template", "P=$(PREFIX), R=$(RECORD), PORT=$(PORT)")
 
 < $(QUADEM)/iocBoot/commonPlugins.cmd
 
 asynSetTraceIOMask("$(PORT)",0,2)
 #asynSetTraceMask("$(PORT)",  0,9)
 
-# initFastSweep(portName, inputName, maxSignals, maxPoints)
-#  portName = asyn port name for this new port (string)
-#  inputName = name of asynPort providing data
-#  maxSignals  = maximum number of signals (spectra)
-#  maxPoints  = maximum number of channels per spectrum
-#  dataString  = drvInfo string for current and position data
-#  intervalString  = drvInfo string for time interval per point
-initFastSweep("$(PORT)TS", "$(PORT)", 11, 2048, "QE_INT_ARRAY_DATA", "QE_SAMPLE_TIME")
-dbLoadRecords("$(QUADEM)/db/quadEM_TimeSeries.template", "P=$(PREFIX),R=$(RECORD)_TS:,NUM_TS=2048,NUM_FREQ=1024,PORT=$(PORT)TS")
-
 < $(QUADEM)/iocBoot/saveRestore.cmd
 
 iocInit()
 
 # save settings every thirty seconds
-create_monitor_set("auto_settings.req",30,"P=$(PREFIX), R=$(RECORD):, R_TS=$(RECORD)_TS:")
-
-seq("quadEM_SNL", "P=$(PREFIX), R=$(RECORD)_TS:, NUM_CHANNELS=2048")
+create_monitor_set("auto_settings.req",30,"P=$(PREFIX), R=$(RECORD)")
