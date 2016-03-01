73e733212f8e7c6269a6e690541e966f9246750d
Mark Rivers <rivers@cars.uchicago.edu>
2016-03-01 11:08:59 -0600

    Put colon in RECORD definition; remove seq command, changes for new NDPluginTimesSeries

--- iocBoot/iocAH401B/AH401B.cmd
+++ iocBoot/iocAH401B/AH401B.cmd
@@ -1,5 +1,5 @@
 epicsEnvSet("PREFIX",    "quadEMTest:")
-epicsEnvSet("RECORD",    "AH401B")
+epicsEnvSet("RECORD",    "AH401B:")
 epicsEnvSet("PORT",      "AH401B")
 epicsEnvSet("TEMPLATE",  "AH401B")
 epicsEnvSet("MODEL",     "AH401B")
@@ -10,14 +10,11 @@ epicsEnvSet("IP",        "164.54.160.242:10001")
 
 < ../AHxxx.cmd
 
-dbLoadRecords("$(QUADEM)/db/AH401B.template", "P=$(PREFIX), R=$(RECORD):, PORT=$(PORT)")
+dbLoadRecords("$(QUADEM)/db/AH401B.template", "P=$(PREFIX), R=$(RECORD), PORT=$(PORT)")
 
 < ../saveRestore.cmd
 
 iocInit()
 
 # save settings every thirty seconds
-create_monitor_set("auto_settings.req",30,"P=$(PREFIX), R=$(RECORD):, R_TS=$(RECORD)_TS:")
-
-seq("quadEM_SNL", "P=$(PREFIX), R=$(RECORD)_TS:, NUM_CHANNELS=2048")
-
+create_monitor_set("auto_settings.req",30,"P=$(PREFIX), R=$(RECORD)")
