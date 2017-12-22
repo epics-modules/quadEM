df7273f5a19a2669f7a648915adb51c01037adef
Mark Rivers <rivers@cars.uchicago.edu>
2017-12-22 08:43:28 -0600

    Set TraceIOTruncateSize; add ADDR and TIMEOUT parameters for template file

--- iocBoot/AHxxx.cmd
+++ iocBoot/AHxxx.cmd
@@ -7,13 +7,14 @@ asynOctetSetOutputEos("IP_$(PORT)", 0, "\r")
 # Set both TRACE_IO_ESCAPE (for ASCII command/response) and TRACE_IO_HEX (for binary data)
 asynSetTraceIOMask("IP_$(PORT)", 0, 6)
 #asynSetTraceFile("IP_$(PORT)",   0, "AHxxx.out")
+#asynSetTraceIOTruncateSize("IP_$(PORT)", 0, 4000)
 #asynSetTraceMask("IP_$(PORT)",   0,  9)
 
 # Load asynRecord record
 dbLoadRecords("$(ASYN)/db/asynRecord.db", "P=$(PREFIX), R=asyn1,PORT=IP_$(PORT),ADDR=0,OMAX=256,IMAX=256")
 
 drvAHxxxConfigure("$(PORT)", "IP_$(PORT)", $(RING_SIZE), $(MODEL))
-dbLoadRecords("$(QUADEM)/db/$(TEMPLATE).template", "P=$(PREFIX), R=$(RECORD), PORT=$(PORT)")
+dbLoadRecords("$(QUADEM)/db/$(TEMPLATE).template", "P=$(PREFIX), R=$(RECORD), PORT=$(PORT), ADDR=0, TIMEOUT=1")
 
 < $(QUADEM)/iocBoot/commonPlugins.cmd
 
