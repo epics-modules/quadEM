f095fef0313a6443ddcdb4a8149d6e2a2363c2ab
Mark Rivers <rivers@cars.uchicago.edu>
2017-12-31 09:58:41 -0600

    Changed to AH501BE; remove dbLoadRecords, no longer needed

--- iocBoot/iocAH501/AH501.cmd
+++ iocBoot/iocAH501/AH501.cmd
@@ -2,14 +2,14 @@ epicsEnvSet("PREFIX",    "quadEMTest:")
 epicsEnvSet("RECORD",    "AH501:")
 epicsEnvSet("PORT",      "AH501")
 epicsEnvSet("TEMPLATE",  "AH501")
-epicsEnvSet("MODEL",     "AH501D")
+#epicsEnvSet("MODEL",     "AH501D")
+epicsEnvSet("MODEL",     "AH501BE")
 epicsEnvSet("QSIZE",     "20")
 epicsEnvSet("RING_SIZE", "10000")
 epicsEnvSet("TSPOINTS",  "1000")
-epicsEnvSet("IP",        "164.54.160.11:10001")
+epicsEnvSet("IP",        "164.54.160.206:10001")
 
 < $(QUADEM)/iocBoot/AHxxx.cmd
-dbLoadRecords("$(QUADEM)/db/AH501.template", "P=$(PREFIX), R=$(RECORD), PORT=$(PORT), ADDR=0, TIMEOUT=1")
 
 < $(QUADEM)/iocBoot/saveRestore.cmd
 
