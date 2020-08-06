fb7281c9e76b7057ac017174d2b4bb31355feff3
Mark Rivers <rivers@cars.uchicago.edu>
2020-08-06 11:15:42 -0500

    New file

---
+++ iocBoot/iocNSLS2_IC/NSLS2_IC.cmd
@@ -0,0 +1,27 @@
+epicsEnvSet("PREFIX",    "quadEMTest:")
+epicsEnvSet("RECORD",    "NSLS2_IC:")
+epicsEnvSet("PORT",      "NSLS2_IC")
+epicsEnvSet("TEMPLATE",  "NSLS2_IC")
+epicsEnvSet("QSIZE",     "20")
+epicsEnvSet("RING_SIZE", "10000")
+epicsEnvSet("TSPOINTS",  "1000")
+epicsEnvSet("MODULE_ID", "0")
+
+drvNSLS2_ICConfigure("$(PORT)", $(MODULE_ID), $(RING_SIZE))
+
+asynSetTraceIOMask("$(PORT)", 0, 2)
+# Uncomment this line to enable asynPrint statements with ASYN_TRACEIO_DRIVER
+asynSetTraceMask("$(PORT)", 0, 9)
+
+dbLoadRecords("$(QUADEM)/db/$(TEMPLATE).template", "P=$(PREFIX), R=$(RECORD), PORT=$(PORT), ADDR=0, TIMEOUT=1")
+dbLoadTemplate("DACs.substitutions")
+
+# Comment out this line to suppress loading the plugins
+< $(QUADEM)/iocBoot/commonPlugins.cmd
+
+< $(QUADEM)/iocBoot/saveRestore.cmd
+
+iocInit()
+
+# save settings every thirty seconds
+create_monitor_set("auto_settings.req",30,"P=$(PREFIX), R=$(RECORD)")
